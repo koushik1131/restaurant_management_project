55a540d382e8c03e4c51c52c3a01672d5786b5de
koushik1131 <168279704+koushik1131@users.noreply.github.com>
2025-09-27 18:40:36 +0530

    51578

---
+++ home/__init__.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.code
+
+import secrets        
+import string
+from django.conf import settings
+from .models import Coupon
+
+ALPHANUMBERIC_CHARS = string.ascii_uppercase + string.digits
+def generate_coupon_code(length=10, max_attempts=10):
+
+    if length <=0:
+        raise valueError("Coupon code length must be greater than zero.")
+
+    for attempt in range(max_attempts):
+        code = ''.join(secrets.choice(ALPHANUMBERIC_CHARS) for _ in range(length))
+
+        if not Coupon.objects.filter(code=code).exists():
+            return code
+
+    raise RuntimeError(
+        f"could not generate a unique coupon code after {max_attempt} attempts."
+        "consider incerasing the length the length or max_attempt."
+    )            
