70f7742b5a03047fa8621fa963cbbc3a637822b2
koushik1131 <168279704+koushik1131@users.noreply.github.com>
2025-09-29 18:48:17 +0530

    51575

--- home/__init__.py
+++ home/__init__.py
@@ -28,3 +28,23 @@ def generate_coupon_code(length=10, max_attempts=10):
         f"could not generate a unique coupon code after {max_attempts} attempts."
         "consider incerasing the length the length or max_attempts."
     )            
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+
+        if not self.pk and not self.code:
+            self.code = generate_coupon_code()
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Coupon"
+        verbose_name_plural = "Coupons"
+
+    def __str__(self):
+        return slef.code        
